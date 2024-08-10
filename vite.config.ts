ed19c6b500aa961732765f0601e9c85904d57658
jo-duchan <jo_duchan@icloud.com>
2024-08-10 12:23:35 +0900

    빌드 관련 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/three-fiber/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
