3f2aa6870c605986ce858e25f3ea476e3b696928
jo-duchan <jo_duchan@icloud.com>
2024-09-20 00:07:48 +0900

    fix: useFrame 'Invalid Hook Call' 에러 개선

--- src/solarSystem/components/CameraController.tsx
+++ src/solarSystem/components/CameraController.tsx
@@ -10,34 +10,32 @@ interface Props {
 function CameraController({ planetRef, isPlanetView }: Props) {
   const { camera } = useThree();
 
-  if (isPlanetView) {
-    useFrame(() => {
-      const current = planetRef?.current;
+  useFrame(() => {
+    const current = planetRef?.current;
 
-      if (current) {
-        // 행성의 위치로 카메라 이동
-        // 행성에서 약간 떨어진 위치에 카메라 배치
-        camera.position.set(
-          current.position.x,
-          current.position.y + 100,
-          current.position.z + 100
-        );
+    if (isPlanetView && current) {
+      // 행성의 위치로 카메라 이동
+      // 행성에서 약간 떨어진 위치에 카메라 배치
+      camera.position.set(
+        current.position.x,
+        current.position.y + 100,
+        current.position.z + 100
+      );
 
-        // 카메라가 태양(0, 0, 0)을 바라보도록 설정
-        camera.lookAt(
-          initialCamera.lookAt.x,
-          initialCamera.lookAt.y,
-          initialCamera.lookAt.z
-        );
-      }
-    });
-  } else {
-    camera.position.set(
-      initialCamera.position.x,
-      initialCamera.position.y,
-      initialCamera.position.z
-    );
-  }
+      // 카메라가 태양(0, 0, 0)을 바라보도록 설정
+      camera.lookAt(
+        initialCamera.lookAt.x,
+        initialCamera.lookAt.y,
+        initialCamera.lookAt.z
+      );
+    } else {
+      camera.position.set(
+        initialCamera.position.x,
+        initialCamera.position.y,
+        initialCamera.position.z
+      );
+    }
+  });
 
   return null;
 }
