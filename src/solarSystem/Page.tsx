f84560008316fcd2cd085523d32b0e102d010aa2
jo-duchan <jo_duchan@icloud.com>
2024-09-26 03:37:23 +0900

    feat: Caption 추가해서 Scale에 대한 안내

--- src/solarSystem/Page.tsx
+++ src/solarSystem/Page.tsx
@@ -1,6 +1,5 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { useControls } from "leva";
 import { initialCamera } from "./constants/cameraData";
 import useCelestialRefs from "./hooks/useCelestialRefs";
 import PlanetOrbit from "./components/PlanetOrbit";
@@ -8,52 +7,33 @@ import CameraController from "./components/CameraController";
 import AxesHelper from "./components/AxesHelper";
 import Light from "./components/Light";
 import CelestialBodies from "./components/CelestialBodies";
+import Caption from "./components/Caption";
 
 export default function SolarSystemPage() {
   const { planets, satellites } = useCelestialRefs();
 
-  const followPlanet = useControls("Follow Planet", {
-    active: false,
-    lookAtPlanet: false,
-    planet: {
-      options: {
-        mercury: planets.mercuryRef,
-        venus: planets.venusRef,
-        earth: planets.earthRef,
-        mars: planets.marsRef,
-        jupiter: planets.jupiterRef,
-        saturn: planets.saturnRef,
-        uranus: planets.uranusRef,
-        neptune: planets.neptuneRef,
-      },
-    },
-    zoom: { value: 100, min: 0, max: 1000, step: 1 },
-  });
-
   return (
-    <Canvas style={{ background: "#101010" }}>
-      <PerspectiveCamera
-        makeDefault
-        position={[
-          initialCamera.position.x,
-          initialCamera.position.y,
-          initialCamera.position.z,
-        ]}
-        fov={initialCamera.for}
-        near={initialCamera.near}
-        far={initialCamera.far}
-      />
-      <OrbitControls enableZoom={true} />
-      <AxesHelper />
-      <Light />
-      <PlanetOrbit {...planets} {...satellites} />
-      <CameraController
-        planetRef={followPlanet.planet}
-        followPlanet={followPlanet.active}
-        lookAtPlanet={followPlanet.lookAtPlanet}
-        zoom={followPlanet.zoom}
-      />
-      <CelestialBodies planets={planets} satellites={satellites} />
-    </Canvas>
+    <>
+      <Canvas style={{ background: "#101010" }}>
+        <PerspectiveCamera
+          makeDefault
+          position={[
+            initialCamera.position.x,
+            initialCamera.position.y,
+            initialCamera.position.z,
+          ]}
+          fov={initialCamera.for}
+          near={initialCamera.near}
+          far={initialCamera.far}
+        />
+        <OrbitControls enableZoom={true} />
+        <AxesHelper />
+        <Light />
+        <PlanetOrbit {...planets} {...satellites} />
+        <CameraController planets={planets} />
+        <CelestialBodies planets={planets} satellites={satellites} />
+      </Canvas>
+      <Caption />
+    </>
   );
 }
---
+++ src/solarSystem/components/Caption.tsx
@@ -0,0 +1,24 @@
+function Caption() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: "20px",
+        right: "20px",
+      }}
+    >
+      <p
+        style={{
+          fontSize: "12px",
+          color: "#ddd",
+          margin: "initial",
+        }}
+      >
+        Scale: Sizes 12,742:1, Distances 38,226:1 (based on Earth's diameter and
+        1 AU).
+      </p>
+    </div>
+  );
+}
+
+export default Caption;
